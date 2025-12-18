21f55f89ac804233463ba842fa13433e559aa1bf
as23ahv <as23ahv@herts.ac.uk>
2025-12-18 23:41:08 +0000

    show patients in gui

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,15 +2,27 @@ package app;
 
 import model.store.DataStore;
 import persistence.FileLoader;
+import view.PatientPanel;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
 
     public static void main(String[] args) {
 
         DataStore store = new DataStore();
-
         FileLoader.loadPatients("data/patients.csv", store);
 
-        System.out.println("Patients loaded: " + store.getPatients().size());
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Healthcare Management System");
+            frame.setSize(900, 500);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.setContentPane(new PatientPanel(store));
+
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
