c596601b74b8e1a080e5bde6d0e94e7ca2f1f699
as23ahv <as23ahv@herts.ac.uk>
2025-12-19 00:18:47 +0000

    loaded clinicians in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,7 +11,12 @@ public class Main {
     public static void main(String[] args) {
 
         DataStore store = new DataStore();
+
         FileLoader.loadPatients("data/patients.csv", store);
+        FileLoader.loadClinicians("data/clinicians.csv", store);
+
+        System.out.println("Patients loaded: " + store.getPatients().size());
+        System.out.println("Clinicians loaded: " + store.getClinicians().size());
 
         SwingUtilities.invokeLater(() -> {
             MainFrame frame = new MainFrame(store);
