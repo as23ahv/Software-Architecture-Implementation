c067430ca3031b9bff2f3c17db359209b99e13f8
as23ahv <as23ahv@herts.ac.uk>
2025-12-19 22:43:39 +0000

    loaded prescriptions in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,17 +12,16 @@ public class Main {
 
         DataStore store = new DataStore();
 
-        // Load CSV data
         FileLoader.loadPatients("data/patients.csv", store);
         FileLoader.loadClinicians("data/clinicians.csv", store);
         FileLoader.loadAppointments("data/appointments.csv", store);
+        FileLoader.loadPrescriptions("data/prescriptions.csv", store);
 
-        // Console checks (useful for debugging & report evidence)
         System.out.println("Patients loaded: " + store.getPatients().size());
         System.out.println("Clinicians loaded: " + store.getClinicians().size());
         System.out.println("Appointments loaded: " + store.getAppointments().size());
+        System.out.println("Prescriptions loaded: " + store.getPrescriptions().size());
 
-        // Launch GUI
         SwingUtilities.invokeLater(() -> {
             MainFrame frame = new MainFrame(store);
             frame.setVisible(true);
