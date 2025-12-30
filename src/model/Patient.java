f2c69f33907c3b599b90c21ffe6c29d9c0aa79cb
as23ahv <as23ahv@herts.ac.uk>
2025-12-30 23:18:58 +0000

    updated patient model to match csv schema

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,69 +1,66 @@
 package model;
 
-/*
- * Represents a patient in the healthcare system.
- * Stores basic demographic and contact information.
- */
 public class Patient {
 
     private String patientId;
     private String firstName;
     private String lastName;
-    private String nhsNumber;
     private String dateOfBirth;
+    private String nhsNumber;
+    private String gender;
     private String phoneNumber;
     private String email;
+    private String address;
+    private String postcode;
+    private String emergencyContactName;
+    private String emergencyContactPhone;
+    private String registrationDate;
     private String gpSurgeryId;
 
-    public Patient(String patientId, String firstName, String lastName,
-                   String nhsNumber, String dateOfBirth,
-                   String phoneNumber, String email, String gpSurgeryId) {
-
+    public Patient(
+            String patientId,
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String nhsNumber,
+            String gender,
+            String phoneNumber,
+            String email,
+            String address,
+            String postcode,
+            String emergencyContactName,
+            String emergencyContactPhone,
+            String registrationDate,
+            String gpSurgeryId
+    ) {
         this.patientId = patientId;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.nhsNumber = nhsNumber;
         this.dateOfBirth = dateOfBirth;
+        this.nhsNumber = nhsNumber;
+        this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.emergencyContactName = emergencyContactName;
+        this.emergencyContactPhone = emergencyContactPhone;
+        this.registrationDate = registrationDate;
         this.gpSurgeryId = gpSurgeryId;
     }
 
-    public String getPatientId() {
-        return patientId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getNhsNumber() {
-        return nhsNumber;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getGpSurgeryId() {
-        return gpSurgeryId;
-    }
-
-    // Useful for debugging and logging
-    @Override
-    public String toString() {
-        return firstName + " " + lastName + " (NHS: " + nhsNumber + ")";
-    }
+    public String getPatientId() { return patientId; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getDateOfBirth() { return dateOfBirth; }
+    public String getNhsNumber() { return nhsNumber; }
+    public String getGender() { return gender; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getEmail() { return email; }
+    public String getAddress() { return address; }
+    public String getPostcode() { return postcode; }
+    public String getEmergencyContactName() { return emergencyContactName; }
+    public String getEmergencyContactPhone() { return emergencyContactPhone; }
+    public String getRegistrationDate() { return registrationDate; }
+    public String getGpSurgeryId() { return gpSurgeryId; }
 }
