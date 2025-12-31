dffa80ebec53426bb6cca053b90a507033a41576
as23ahv <as23ahv@herts.ac.uk>
2025-12-31 01:13:25 +0000

    expanded staff table columns

--- src/view/StaffPanel.java
+++ src/view/StaffPanel.java
@@ -9,22 +9,34 @@ import javax.swing.table.DefaultTableModel;
 public class StaffPanel extends JPanel {
 
     public StaffPanel(DataStore store) {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        String[] columns = {"Staff ID", "Name", "Role", "Department", "Facility ID"};
-        DefaultTableModel tableModel = new DefaultTableModel(columns, 0);
+        String[] cols = {
+                "Staff ID", "First Name", "Last Name", "Role", "Department",
+                "Facility ID", "Phone", "Email", "Employment Status",
+                "Start Date", "Line Manager", "Access Level"
+        };
+
+        DefaultTableModel tableModel = new DefaultTableModel(cols, 0);
 
         for (Staff s : store.getStaff().values()) {
             tableModel.addRow(new Object[]{
                     s.getStaffId(),
-                    s.getFullName(),
+                    s.getFirstName(),
+                    s.getLastName(),
                     s.getRole(),
                     s.getDepartment(),
-                    s.getFacilityId()
+                    s.getFacilityId(),
+                    s.getPhoneNumber(),
+                    s.getEmail(),
+                    s.getEmploymentStatus(),
+                    s.getStartDate(),
+                    s.getLineManager(),
+                    s.getAccessLevel()
             });
         }
 
         JTable table = new JTable(tableModel);
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(new JScrollPane(table));
     }
 }
