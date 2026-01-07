54779690cf7faceb993305a9bd32bc8d999d36d3
as23ahv <as23ahv@herts.ac.uk>
2026-01-07 13:19:29 +0000

    added person base class

--- src/model/Person.java
+++ src/model/Person.java
@@ -2,20 +2,17 @@ package model;
 
 public abstract class Person {
 
-    private String personId;
+    private final String personId;
     private String firstName;
     private String lastName;
     private String phoneNumber;
-    private String address;
     private String email;
 
-    public Person(String personId, String firstName, String lastName,
-                  String phoneNumber, String address, String email) {
+    public Person(String personId, String firstName, String lastName, String phoneNumber, String email) {
         this.personId = personId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
-        this.address = address;
         this.email = email;
     }
 
@@ -35,20 +32,20 @@ public abstract class Person {
         return phoneNumber;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public void setEmail(String email) {
